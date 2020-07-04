6fb5aeb53fe6738c9c66fb1845090f8ed0e20f8e
Jennifer Jang <j.jang42@gmail.com>
2020-07-04 17:30:04 -0400

    Docstring consistency

--- sklearn/neighbors/_subsampled.py
+++ sklearn/neighbors/_subsampled.py
@@ -78,14 +78,14 @@ class SubsampledNeighborsTransformer(TransformerMixin, UnsupervisedMixin,
         Parameters
         ----------
         X : array-like of shape (n_samples, n_features)
-                Sample data.
+            Sample data.
 
         Returns
         -------
         neighborhood : sparse matrix of shape (n_samples, n_samples)
-                Non-zero entries in neighborhood[i, j] indicate an edge 
-                between X[i] and X[j] with value equal to weight of edge.
-                The matrix is of CSR format.
+            Non-zero entries in neighborhood[i, j] indicate an edge 
+            between X[i] and X[j] with value equal to weight of edge.
+            The matrix is of CSR format.
         """
 
         check_is_fitted(self)
@@ -99,15 +99,15 @@ class SubsampledNeighborsTransformer(TransformerMixin, UnsupervisedMixin,
         Parameters
         ----------
         X : array-like of shape (n_samples, n_features)
-                Sample data.
+            Sample data.
         y : ignored
 
         Returns
         -------
         neighborhood : sparse matrix of shape (n_samples, n_samples)
-                Non-zero entries in neighborhood[i, j] indicate an edge 
-                between X[i] and X[j] with value equal to weight of edge.
-                The matrix is of CSR format.
+            Non-zero entries in neighborhood[i, j] indicate an edge 
+            between X[i] and X[j] with value equal to weight of edge.
+            The matrix is of CSR format.
         """
         
         return self.fit(X).transform(X)
@@ -139,9 +139,9 @@ class SubsampledNeighborsTransformer(TransformerMixin, UnsupervisedMixin,
         Returns
         -------
         neighborhood : sparse matrix of shape (n_samples, n_samples)
-                Non-zero entries in neighborhood[i, j] indicate an edge 
-                between X[i] and X[j] with value equal to weight of edge.
-                The matrix is of CSR format.
+            Non-zero entries in neighborhood[i, j] indicate an edge 
+            between X[i] and X[j] with value equal to weight of edge.
+            The matrix is of CSR format.
         """
 
         from scipy.sparse import csr_matrix
