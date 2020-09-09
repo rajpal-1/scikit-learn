59b2656ef9fca9dbd9bbfefb90f9fbaf05a7542c
Jennifer Jang <j.jang42@gmail.com>
2020-09-09 14:39:18 -0400

    Example file text

--- examples/cluster/plot_subsampled_neighbors_transformer_dbscan.py
+++ examples/cluster/plot_subsampled_neighbors_transformer_dbscan.py
@@ -1,16 +1,9 @@
 """
 ====================================================================
-Comparison of the K-Means and MiniBatchKMeans clustering algorithms
+Comparison of DBSCAN and DBSCAN with Subsampling
 ====================================================================
 
-We want to compare the performance of the MiniBatchKMeans and KMeans:
-the MiniBatchKMeans is faster, but gives slightly different results (see
-:ref:`mini_batch_kmeans`).
-
-We will cluster a set of data, first with KMeans and then with
-MiniBatchKMeans, and plot the results.
-We will also plot the points that are labelled differently between the two
-algorithms.
+We want to compare the performance of DBSCAN and DBSCAN with subsampling.
 """
 print(__doc__)
 
